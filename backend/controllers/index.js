ae596074b34855eb86e3e782f6eaf699a1fdda1f
Gourav Saini <mailtogouravsaini@gmail.com>
2021-09-17 19:44:32 +0530

    s3 upload url

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import users from './users';
 import ping from './ping';
 import news from "./news";
+import uploadURL from "./uploadURL";
 
 const controllers = express.Router();
 
 controllers.use('/users', users);
 controllers.use('/news', news);
 controllers.get('/ping', ping);
+controllers.post('/upload', uploadURL);
 
 export default controllers;
