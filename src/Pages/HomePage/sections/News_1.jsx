4c9b5d319b163ab60ee3329d50011820e876c1c3
mohit <naruka.mohit88@gmail.com>
2022-07-17 21:16:10 +0530

    updated

---
+++ src/Pages/HomePage/sections/News_1.jsx
@@ -0,0 +1,82 @@
+import React,{useState,useEffect}from 'react';
+import { listNews, NewsType } from "../../../utils/firebase/news";
+import "./News_1.css";
+import { NewsComponent } from './News';
+import Linkify from 'react-linkify';
+import styled from "styled-components";
+
+function News_1() {
+  const [newsList, setNewsList] = useState([]);
+  useEffect(() => {
+    try {
+      listNews().then((data) => {
+        setNewsList(data);
+      });
+    } catch (error) { }
+  }, []);
+  return (
+    <div class="news_1">
+        <div class="box1" style={{width:'100%',height:'5vmax',backgroundColor:"#F4C430"}} >
+                <div class='marquee' >
+                    <div class='marqueeContentContainer'>
+                    {newsList
+              ?.filter((item) => item.type == "new")
+              // .slice(0, 2)
+              .map((item) => (
+                <NewsComponent
+                  news={item}
+                  link
+                  width={
+                    newsList?.filter((item) => item.type == "new").length === 1
+                      ? "100%"
+                      : "50%"
+                  }
+                />
+              ))}
+                    </div>
+
+                </div>
+                
+        </div>
+        <div class="box1" style={{width:'100%',height:'5vmax',backgroundColor:"#fff"}}>
+                <div class='marquee' >
+                    <div class='marqueeContentContainer' >
+                    {newsList
+              ?.filter((item) => item.type == "usefulLinks")
+              .map((item) => (
+                <NewsContainer>
+                  <a target={"_blank"} href={item.links[0]}>
+                    {item.title}
+                  </a>
+                </NewsContainer>
+              ))}
+                    </div>
+
+                </div>
+                
+        </div>
+        <div class="box1" style={{width:'100%',height:'5vmax',backgroundColor:"#1FAA59"}}>
+                <div class='marquee' >
+                    <div class='marqueeContentContainer' >
+                      
+                    </div>
+
+                </div>
+                
+        </div>
+    </div>
+  )
+}
+
+const NewsContainer = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  a {
+    font-size: 1rem;
+    margin: 10px 0;
+    display: block;
+    text-decoration: none;
+  }
+`;
+
+export default News_1;
